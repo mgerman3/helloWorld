060fe2d5c83f89506a4cafaed4d792188201d294
michaelgerman <mgerman@terpmail.umd.edu>
2023-03-27 21:41:59 -0400

    update to module 8

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! From Michael German. I am adding my first code change.'
+    return 'Hello World!- From Michael German. I am adding my first code change.'
 
 @app.route('/hello')
 def hello():  # put application's code here
@@ -14,6 +14,22 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('Subject: ' + request.args.get('subject'))
+    print('Number: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+
+@app.route('/contact')
+def contact():
+    if request.method == 'post':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
